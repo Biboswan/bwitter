08e5716ad4202b8db8fa1f3a5d2dbf8aad590878
Biboswan Roy <biboswan98@gmail.com>
2022-06-28 06:58:04 +0530

    remove express server

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,9 @@
-import express from 'express';
 import fillDummyData from './fillDummyData.js';
 import loadSchema from './loadSchema.js';
-
-const app = express();
-const PORT = 5000;
 
 const main = async () => {
     await loadSchema();
     await fillDummyData();
-    app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
 };
 
 main();
